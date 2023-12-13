29b681e1fd099e80186bce90871384dce9a57b27
Renato Maldonado <mallldonado@hotmail.com>
2023-12-13 17:13:28 +0000

    add roles to filters by user and fixes on frontend

--- backend/app/Http/Middleware/KeycloakAuthMiddleware.php
+++ backend/app/Http/Middleware/KeycloakAuthMiddleware.php
@@ -17,11 +17,11 @@ class KeycloakAuthMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        // if(!Auth::check()){
-        //     return response()->json([
-        //         'message' => 'Unauthorized'
-        //     ], 401);
-        // }
+        if(!Auth::check()){
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
         return $next($request);
     }
 }
